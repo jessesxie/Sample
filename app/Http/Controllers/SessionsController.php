5a4138241ed825d6c5296ecd3a4eaa72f8d8c728
xie <2306352258@qq.com>
2017-03-08 15:25:03 +0000

    Finish user edit, update, index, add destroy actions

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -11,6 +11,13 @@ use Auth;
 
 class SessionsController extends Controller
 {
+    public function __construct()
+    {
+      $this->middleware('guest', [
+        'only' => ['create']
+      ]);
+    }
+  
     public function create()
     {
       return view('sessions.create');
@@ -31,7 +38,7 @@ class SessionsController extends Controller
       if (Auth::attempt($credentials, $request->has('remember'))) {
         //succeed
         session()->flash('success', 'welcome back!');
-        return redirect()->route('users.show', [Auth::user()]);
+        return redirect()->intended(route('users.show', [Auth::user()]));
       } else {
         //fail
         session()->flash('danger', 'sorry, your eamil or password is wrong!');
